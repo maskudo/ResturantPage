9e980defcbf2ebe36d59201c1aa75a51ebcac85f
maskudo <manujkarki101@gmail.com>
2021-10-16 15:00:12 +0545

    added images folder to dist

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+                generator: {
+                  filename: 'images/[hash][ext][query]'
+                }
             }
 
         ]
