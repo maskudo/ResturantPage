fb2a4eed895a2aa705e226555e506d65338a64c3
maskudo <manujkarki101@gmail.com>
2022-08-23 12:36:38 +0545

    replace jpgs with pngs

--- src/index.js
+++ src/index.js
@@ -2,105 +2,103 @@ import "./style.css";
 import { renderHome } from "./home.js";
 import { renderMenu } from "./menu.js";
 import { renderContact } from "./contact";
-import background from "./img/emberBackground.jpg"
-import dungPie from "./img/dungPie.png"
-import estus from "./img/estus.png"
+import background from "./img/emberBackground.jpg";
+import dungPie from "./img/dungPie.png";
+import estus from "./img/estus.png";
 
-const content = document.querySelector('#content');
-const body = document.querySelector("body")
-body.style.backgroundImage = `url(${background})`
+const content = document.querySelector("#content");
+const body = document.querySelector("body");
+body.style.backgroundImage = `url(${background})`;
 
-function createHeader(){
-    const header = document.createElement('header');
-    const resturantName = document.createElement('h1');
-    const bonfireImg1 = document.createElement('img')
-    const bonfireImg2 = document.createElement('img')
-    bonfireImg1.src = dungPie
-    bonfireImg2.src = estus
-    bonfireImg1.classList.add("headerImg")
-    bonfireImg2.classList.add("headerImg")
-    bonfireImg2.classList.add("headerImg2")
-    
-    resturantName.textContent = 'Firelink Dine';
-    header.appendChild(bonfireImg1);
-    header.appendChild(resturantName);
-    header.appendChild(bonfireImg2)
+function createHeader() {
+  const header = document.createElement("header");
+  const resturantName = document.createElement("h1");
+  const bonfireImg1 = document.createElement("img");
+  const bonfireImg2 = document.createElement("img");
+  bonfireImg1.src = dungPie;
+  bonfireImg2.src = estus;
+  bonfireImg1.classList.add("headerImg");
+  bonfireImg2.classList.add("headerImg");
+  bonfireImg2.classList.add("headerImg2");
 
-    content.appendChild(header);
+  resturantName.textContent = "Firelink Dine";
+  header.appendChild(bonfireImg1);
+  header.appendChild(resturantName);
+  header.appendChild(bonfireImg2);
+
+  content.appendChild(header);
 }
-function createFooter(){
-    const footer = document.createElement("footer")
-    const copyright = document.createElement('p')
-    const link = document.createElement('a')
+function createFooter() {
+  const footer = document.createElement("footer");
+  const copyright = document.createElement("p");
+  const link = document.createElement("a");
 
-    copyright.textContent = "Copyright © 2021"
-    link.text = "maskudo"
-    link.href = "https://github.com/maskudo"
+  copyright.textContent = "Copyright © 2021";
+  link.text = "maskudo";
+  link.href = "https://github.com/maskudo";
 
-    footer.append(copyright)
-    footer.append(link)
+  footer.append(copyright);
+  footer.append(link);
 
-    content.append(footer)
+  content.append(footer);
 }
-function createNav(){
-    const nav = document.createElement('nav');
-    nav.classList.add('navBox');
-    
-    const home = document.createElement('button');
-    home.textContent = 'Home';
-    home.id = 'active';
-    home.classList.add('active')
-    nav.append(home);
+function createNav() {
+  const nav = document.createElement("nav");
+  nav.classList.add("navBox");
 
-    home.addEventListener('click',e => {
-        setActive(home);
-        content.append(renderHome());
-        createFooter()
-    })
+  const home = document.createElement("button");
+  home.textContent = "Home";
+  home.id = "active";
+  home.classList.add("active");
+  nav.append(home);
 
-    const menu = document.createElement('button');
-    menu.textContent = 'Menu';
-    menu.id = 'menu';
-    nav.append(menu);
-    menu.addEventListener('click',e => {
-        setActive(menu);
-        content.append(renderMenu());
-        createFooter()
-    })
-
-    const contact = document.createElement('button');
-    contact.textContent = 'Contact';
-    contact.id = 'contact';
-    nav.append(contact);
-    contact.addEventListener('click',e => {
-        setActive(contact);
-        content.append(renderContact());
-        createFooter()
-    })
-
-    content.append(nav);
-    
-}
-function initMainContent()
-{
+  home.addEventListener("click", (e) => {
+    setActive(home);
     content.append(renderHome());
+    createFooter();
+  });
+
+  const menu = document.createElement("button");
+  menu.textContent = "Menu";
+  menu.id = "menu";
+  nav.append(menu);
+  menu.addEventListener("click", (e) => {
+    setActive(menu);
+    content.append(renderMenu());
+    createFooter();
+  });
+
+  const contact = document.createElement("button");
+  contact.textContent = "Contact";
+  contact.id = "contact";
+  nav.append(contact);
+  contact.addEventListener("click", (e) => {
+    setActive(contact);
+    content.append(renderContact());
+    createFooter();
+  });
+
+  content.append(nav);
+}
+function initMainContent() {
+  content.append(renderHome());
 }
 
-function setActive(navButton){
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(button=>{
-        if(button.classList == 'active'){
-            button.classList.remove('active');
-            button.id = ""
-        }
-    })
-    const mainContent = content.querySelector('.mainContent')
-    content.removeChild(mainContent)
-    const footer = content.querySelector("footer")
-    content.removeChild(footer)
-    
-    navButton.classList.add('active')
-    navButton.id = "active"
+function setActive(navButton) {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    if (button.classList == "active") {
+      button.classList.remove("active");
+      button.id = "";
+    }
+  });
+  const mainContent = content.querySelector(".mainContent");
+  content.removeChild(mainContent);
+  const footer = content.querySelector("footer");
+  content.removeChild(footer);
+
+  navButton.classList.add("active");
+  navButton.id = "active";
 }
 
 createHeader();
--- src/menu.js
+++ src/menu.js
@@ -1,50 +1,50 @@
 import Estus from "./img/estus.png";
 import bloodRed from "./img/bloodredMossClump.png";
-import bloomingPurple from "./img/bloomingPurpleMossClump.jpg";
-import divineBlessing from "./img/divineBlessing.jpg";
+import bloomingPurple from "./img/bloomingPurpleMossClump.png";
+import divineBlessing from "./img/divineBlessing.png";
 import greenBlossom from "./img/greenBlossom.png";
 import dungPie from "./img/dungPie.png";
 
-function createMenuItem(source,name){
-    const menuItem = document.createElement('div')
-    const itemName = document.createElement('h2')
-    const itemImage = document.createElement('img')
+function createMenuItem(source, name) {
+  const menuItem = document.createElement("div");
+  const itemName = document.createElement("h2");
+  const itemImage = document.createElement("img");
 
-    menuItem.className = 'menu-item'
+  menuItem.className = "menu-item";
 
-    itemImage.src = source
-    itemName.textContent = name
-    
-    menuItem.append(itemImage)
-    menuItem.append(itemName)
+  itemImage.src = source;
+  itemName.textContent = name;
 
-    return menuItem
+  menuItem.append(itemImage);
+  menuItem.append(itemName);
+
+  return menuItem;
 }
-export function renderMenu(){
-    const menu = document.createElement('div')
-    menu.classList.add('mainContent')
-    menu.classList.add('menu')
-    
-    const beverages = document.createElement('h1')
-    const mainDish = document.createElement('h1')
+export function renderMenu() {
+  const menu = document.createElement("div");
+  menu.classList.add("mainContent");
+  menu.classList.add("menu");
 
-    beverages.textContent = "Beverages"
-    mainDish.textContent = "Main Dishes"
+  const beverages = document.createElement("h1");
+  const mainDish = document.createElement("h1");
 
-    const item1 = createMenuItem(Estus,"Estus")
-    const item2 = createMenuItem(divineBlessing, "Diving Blessing")
-    const item3 = createMenuItem(dungPie,"Dung Pie")
-    const item4 = createMenuItem(bloomingPurple,"Blooming Purple Moss Clump")
-    const item5 = createMenuItem(greenBlossom,"Green Blossom")
-    const item6 = createMenuItem(bloodRed,'Bloodred Moss Clump')
+  beverages.textContent = "Beverages";
+  mainDish.textContent = "Main Dishes";
 
-    menu.append(beverages)
-    menu.append(item1)
-    menu.append(item2)
-    menu.append(mainDish)
-    menu.append(item3)
-    menu.append(item4)
-    menu.append(item5)
-    menu.append(item6)
-    return menu;
+  const item1 = createMenuItem(Estus, "Estus");
+  const item2 = createMenuItem(divineBlessing, "Diving Blessing");
+  const item3 = createMenuItem(dungPie, "Dung Pie");
+  const item4 = createMenuItem(bloomingPurple, "Blooming Purple Moss Clump");
+  const item5 = createMenuItem(greenBlossom, "Green Blossom");
+  const item6 = createMenuItem(bloodRed, "Bloodred Moss Clump");
+
+  menu.append(beverages);
+  menu.append(item1);
+  menu.append(item2);
+  menu.append(mainDish);
+  menu.append(item3);
+  menu.append(item4);
+  menu.append(item5);
+  menu.append(item6);
+  return menu;
 }
